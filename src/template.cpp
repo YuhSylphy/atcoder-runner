db9d40a28867999f4277f38fad2b43b927c86956
YuhSylphy <yuhsylphy@gmail.com>
2020-05-23 20:16:24 +0900

    Add template.cpp

---
+++ src/template.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+int main() {
+	ll N;
+	cin >> N;
+
+	auto ans = 0;
+
+	cout << ans << endl;
+
+	return 0;
+}
